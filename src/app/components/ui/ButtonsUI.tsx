51e83a6cfbcc92179b28f0b108c07d2c76ce1f61
S4m0htTT <thomashenry7750@gmail.com>
2024-11-08 11:32:18 +0100

    feature 109: Implementation of a concise and clear color theme (naming) in the tailwind config file.

--- src/app/components/ui/ButtonsUI.tsx
+++ src/app/components/ui/ButtonsUI.tsx
@@ -6,7 +6,7 @@ function ButtonsUI({ type, label }: ButtonsUIProps) {
   const buttonClasses = classNames(
     "w-72 rounded-[10px] py-[10px] font-tahoma",
     {
-      "bg-principale-blue text-background-color hover:bg-dark-blue transition-colors duration-200":
+      "bg-principale-blue text-background-color hover:bg-principal-blue-hover transition-colors duration-200":
         type === "submit",
       "bg-gray-500 text-gray-200": type === "button",
     }
