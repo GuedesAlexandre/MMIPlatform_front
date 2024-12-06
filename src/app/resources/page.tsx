8fb0076d302811c296ab95a02bb3031edd01c1bc
DuskFried <jerome.fabre77@gmail.com>
2024-12-06 03:56:31 +0100

    feat: card ressource

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import ResourceCard from "@/app/components/resourcesCard"
-import { useAuthStore } from "@/app/store/AuthRepository"
-import TitleHeaderUI from "@/app/components/ui/TitleHeaderUI"
+import ResourceCard from "@/app/components/resourcesCard";
+import { useAuthStore } from "@/app/store/AuthRepository";
+import TitleHeaderUI from "@/app/components/ui/TitleHeaderUI";
 
 const Resources = () => {
   const { user } = useAuthStore();
   return (
     <div>
       <TitleHeaderUI label="Mes Ressources" />
-      <div className="w-[90%] mx-auto">
+      <div className="p-10 grid grid-cols-3 mx-auto gap-10 flex-wrap">
         {user &&
           user.user.moduleDaos.map((module) => {
             return <ResourceCard key={module.id} module={module} />;
