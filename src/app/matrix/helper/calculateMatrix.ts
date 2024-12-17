8f68356d204dbe51f4e19a06d695faa65611f424
S4m0htTT <thomashenry7750@gmail.com>
2024-12-17 11:55:34 +0100

    Feature (416) : Start algo matrix

--- src/app/matrix/helper/calculateMatrix.ts
+++ src/app/matrix/helper/calculateMatrix.ts
@@ -6,18 +6,44 @@ export const groupeUtilsInformation = (
   semester: string,
   ue: ueModule
 ) => {
-  const studentsFiltredBySemester = getStudentBySemester(students, semester);
+  const studentsFiltredBySemester = getStudentBySemester(
+    students,
+    semester,
+    ue
+  );
+  return studentsFiltredBySemester;
 };
 
 const getStudentBySemester = (
   students: Student[] | undefined,
-  semester: string
+  semester: string,
+  ue: ueModule
 ) => {
   if (!students) return [];
   const studentWithNote = students.filter(
     (student) => student.notes.length !== 0
   );
-  return studentWithNote.filter((student) =>
-    student.notes.find((note) => note.module.semester === semester)
+  const filteredStudentNote = studentWithNote.filter(
+    (student) =>
+      student.notes.find((note) => note.module.semester === semester) &&
+      student.notes.find((note) => note.module.ueName === ue)
   );
+  // return filteredStudentNote;
+  return filteredStudentNote.map((notes)=> {
+    return {
+      "name": notes.firstName + " " + notes.lastName,
+      "group": notes.group,
+      "numEtu": notes.numEtu,
+      "note": [
+        notes.notes.map((note) => {
+          return {
+            "resource": note.module.name,
+            "note": note.note
+          }
+        })
+      ],
+      "average": 0,
+      "results": "ADMIS"
+    }
+  })
 };
