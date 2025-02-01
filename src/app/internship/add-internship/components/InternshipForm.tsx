eb4e61085502fe707d9f6fe440da4842e52f9904
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2025-02-01 18:38:55 +0100

    feat(SAE5-132): WIP

---
+++ src/app/internship/add-internship/components/InternshipForm.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import { useForm, FormProvider, SubmitHandler, Controller } from 'react-hook-form';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Internship } from '@/app/models/Internship';
+import { Select, SelectItem, SelectTrigger, SelectContent, SelectValue } from '@/components/ui/select';
+
+interface InternshipFormProps {
+  onSubmit: SubmitHandler<Internship>;
+}
+
+const InternshipForm: React.FC<InternshipFormProps> = ({ onSubmit }) => {
+  const methods = useForm<Internship>();
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <FormField
+          control={methods.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Intitulé du stage</FormLabel>
+              <FormControl>
+                <Input placeholder="Intitulé du stage" {...field} />
+              </FormControl>
+              <FormMessage>
+                {methods.formState.errors.title && "Ce champ est obligatoire"}
+              </FormMessage>
+            </FormItem>
+          )}
+        />
+          <FormField
+          control={methods.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Type d&apos;expériences</FormLabel>
+              <FormControl>
+            <Controller
+              control={methods.control}
+              name="type"
+              render={({ field }) => (
+                <Select onValueChange={field.onChange} value={field.value}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Sélectionner un type d'expériences" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="STAGE">Stage</SelectItem>
+                    <SelectItem value="ALTERNANCE">Alternance</SelectItem>
+                  </SelectContent>
+                </Select>
+              )}
+            />
+              </FormControl>
+              <FormMessage>
+                {methods.formState.errors.weekCount && "Ce champ est obligatoire"}
+              </FormMessage>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={methods.control}
+          name="comment"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Commentaries</FormLabel>
+              <FormControl>
+                <Input placeholder="Commentaries" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={methods.control}
+          name="weekCount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nombre de semaines</FormLabel>
+              <FormControl>
+                <Input
+                  type="number"
+                  placeholder="Nombre de semaines"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage>
+                {methods.formState.errors.weekCount && "Ce champ est obligatoire"}
+              </FormMessage>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={methods.control}
+          name="years"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Année</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="Année" {...field} />
+              </FormControl>
+              <FormMessage>
+                {methods.formState.errors.years && "Ce champ est obligatoire"}
+              </FormMessage>
+            </FormItem>
+          )}
+        />
+        <div className="flex space-x-4">
+          <button
+            className="px-4 py-2 my-4 bg-primary-blue text-white rounded hover:bg-primary-blue-hover transition-colors duration-300"
+            type="submit"
+          >
+            Ajouter un stage
+          </button>
+          <button
+            className="px-4 py-2 my-4 bg-red-500 text-white rounded hover:bg-red-700 transition-colors duration-300"
+            type="button"
+            onClick={() => methods.reset()}
+          >
+            Réinitialiser
+          </button>
+        </div>
+      </form>
+    </FormProvider>
+  );
+};
+
+export default InternshipForm;
--- src/app/internship/add-internship/page.tsx
+++ src/app/internship/add-internship/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import TitleHeaderUI from "@/app/components/ui/TitleHeaderUI";
-import { Internship } from "@/app/models/Internship";
 import { useInternshipStore } from "@/app/store/Internship.store";
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
-import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form";
+import { Internship } from "@/app/models/Internship";
+import InternshipForm from "@/app/internship/add-internship/components/InternshipForm";
+import Accordion from "@/app/components/accordion";
+import { PersonIcon } from "@radix-ui/react-icons";
 
 const PostInternship = () => {
   const param = useSearchParams();
@@ -14,11 +15,9 @@ const PostInternship = () => {
   const { addInternship } = useInternshipStore();
   const router = useRouter();
   if (!numEtu) router.back();
-  const methods = useForm<Internship>();
 
   const onSubmit: SubmitHandler<Internship> = (data) => {
-    console.log(data);
-    if (numEtu) addInternship(numEtu, data).then(()=>{
+    if (numEtu) addInternship(numEtu, data).then(() => {
       router.push(`/internship/internship-management/?numEtu=${numEtu}`);
     });
   };
@@ -26,84 +25,15 @@ const PostInternship = () => {
   return (
     <>
       <TitleHeaderUI label="Ajouter un stage" />
-      <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <FormField
-            control={methods.control}
-            name="title"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Intitulé du stage</FormLabel>
-                <FormControl>
-                  <Input placeholder="Intitulé du stage" {...field} />
-                </FormControl>
-                <FormMessage>
-                  {methods.formState.errors.title && "Ce champ est obligatoire"}
-                </FormMessage>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={methods.control}
-            name="comment"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Commentaries</FormLabel>
-                <FormControl>
-                  <Input placeholder="Commentaries" {...field} />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={methods.control}
-            name="type"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Type (STAGE ou ALTERNANCE)</FormLabel>
-                <FormControl>
-                  <Input placeholder="Type de contrat" {...field} />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={methods.control}
-            name="weekCount"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Nombre de semaines</FormLabel>
-                <FormControl>
-                  <Input
-                    type="number"
-                    placeholder="Nombre de semaines"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage>
-                  {methods.formState.errors.weekCount && "Ce champ est obligatoire"}
-                </FormMessage>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={methods.control}
-            name="years"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Année</FormLabel>
-                <FormControl>
-                  <Input type="number" placeholder="Année" {...field} />
-                </FormControl>
-                <FormMessage>
-                  {methods.formState.errors.years && "Ce champ est obligatoire"}
-                </FormMessage>
-              </FormItem>
-            )}
-          />
-          <button type="submit">Ajouter un stage</button>
-        </form>
-      </FormProvider>
+      <div className="p-10 mx-auto">
+      <Accordion
+        icon={<PersonIcon />}
+        name={"Ajouter un stage"}
+        open={false}
+        data={
+          <InternshipForm onSubmit={onSubmit} />
+        }
+      /></div>
     </>
   );
 };
