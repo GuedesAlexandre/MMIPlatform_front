98e997b44dbc9ffb4f6e25471484ae22cc815d8c
Alzower <glenn.guillard.pros@gmail.com>
2025-03-25 12:26:31 +0100

    feat(--no-ticket) : little fix

--- src/app/internship/list-student/page.tsx
+++ src/app/internship/list-student/page.tsx
@@ -1,49 +1,46 @@
-"use client";
-import Accordion from "@/app/components/accordion";
-import TitleHeaderUI from "@/app/components/ui/TitleHeaderUI";
-import { useStudentsByPromo } from "@/app/store/useStudentsByPromo.store";
-import { ArrowLeftIcon, PersonIcon } from "@radix-ui/react-icons";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import DataStudentTable from "./components/DataStudentTable";
+'use client'
+import Accordion from '@/app/components/accordion'
+import TitleHeaderUI from '@/app/components/ui/TitleHeaderUI'
+import { useStudentsByPromo } from '@/app/store/useStudentsByPromo.store'
+import { ArrowLeftIcon, PersonIcon } from '@radix-ui/react-icons'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import DataStudentTable from './components/DataStudentTable'
 
 const ListStudent = () => {
-  const searchParm = useSearchParams();
-  const promo = searchParm.get("promo");
-  const { studentsByPromo, setStudentByPromo } = useStudentsByPromo();
-  const router = useRouter();
+  const searchParm = useSearchParams()
+  const promo = searchParm.get('promo')
+  const { studentsByPromo, setStudentByPromo } = useStudentsByPromo()
+  const router = useRouter()
   useEffect(() => {
-    if (!promo) return;
-    setStudentByPromo(promo);
-  }, [promo, setStudentByPromo]);
-
+    if (!promo) return
+    setStudentByPromo(promo)
+  }, [promo, setStudentByPromo])
 
   return (
     <>
-      <TitleHeaderUI
-        label={`Gestion de la scolarité et des absences ${promo}`}
-      ></TitleHeaderUI>
-       <div
+      <TitleHeaderUI label={`Gestion des stages ${promo}`}></TitleHeaderUI>
+      <div
         onClick={() => router.back()}
-        className="flex flex-row items-center ml-10 cursor-pointer hover:underline w-fit pt-10"
+        className='flex flex-row items-center ml-10 cursor-pointer hover:underline w-fit pt-10'
       >
-        <ArrowLeftIcon className="size-6 mr-1" />
+        <ArrowLeftIcon className='size-6 mr-1' />
         <p>Retour</p>
       </div>
-      <div className="p-10 mx-auto">
-      <Accordion
-        icon={<PersonIcon />}
-        name={"Liste des étudiants"}
-        open={true}
-        data={
-          studentsByPromo && (
-            <DataStudentTable studentsByPromo={studentsByPromo} />
-          )
-        }
-      />
+      <div className='p-10 mx-auto'>
+        <Accordion
+          icon={<PersonIcon />}
+          name={'Liste des étudiants'}
+          open={true}
+          data={
+            studentsByPromo && (
+              <DataStudentTable studentsByPromo={studentsByPromo} />
+            )
+          }
+        />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default ListStudent;
+export default ListStudent
--- src/app/internship/put-internship/components/InternshipForm.tsx
+++ src/app/internship/put-internship/components/InternshipForm.tsx
@@ -1,121 +1,147 @@
-import React, { useEffect } from 'react';
-import { useForm, FormProvider, SubmitHandler, Controller } from 'react-hook-form';
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Internship } from '@/app/models/Internship';
-import { Select, SelectItem, SelectTrigger, SelectContent, SelectValue } from '@/components/ui/select';
+import React, { useEffect } from 'react'
+import {
+  useForm,
+  FormProvider,
+  SubmitHandler,
+  Controller,
+} from 'react-hook-form'
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Internship } from '@/app/models/Internship'
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+} from '@/components/ui/select'
 
 interface InternshipPutFormProps {
-  onSubmit: SubmitHandler<Internship>;
-  internshipToPut ?: Internship;
+  onSubmit: SubmitHandler<Internship>
+  internshipToPut?: Internship
 }
 
-const InternshipPutForm: React.FC<InternshipPutFormProps> = ({ onSubmit, internshipToPut }) => {
-  const methods = useForm<Internship>();
+const InternshipPutForm: React.FC<InternshipPutFormProps> = ({
+  onSubmit,
+  internshipToPut,
+}) => {
+  const methods = useForm<Internship>()
 
   useEffect(() => {
-    methods.reset(internshipToPut);
-  }, [internshipToPut, methods]);
+    methods.reset(internshipToPut)
+  }, [internshipToPut, methods])
 
   return (
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)}>
         <FormField
           control={methods.control}
-          name="title"
+          name='title'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Intitulé du stage</FormLabel>
               <FormControl>
-                <Input placeholder="Intitulé du stage" {...field} />
+                <Input placeholder='Intitulé du stage' {...field} />
               </FormControl>
               <FormMessage>
-                {methods.formState.errors.title && "Ce champ est obligatoire"}
+                {methods.formState.errors.title && 'Ce champ est obligatoire'}
               </FormMessage>
             </FormItem>
           )}
         />
         <FormField
           control={methods.control}
-          name="type"
+          name='type'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Type d&apos;expériences</FormLabel>
               <FormControl>
                 <Controller
                   control={methods.control}
-                  name="type"
+                  name='type'
                   render={({ field }) => (
                     <Select onValueChange={field.onChange} value={field.value}>
                       <SelectTrigger>
                         <SelectValue placeholder="Sélectionner un type d'expériences" />
                       </SelectTrigger>
                       <SelectContent>
-                        <SelectItem value="STAGE">Stage</SelectItem>
-                        <SelectItem value="ALTERNANCE">Alternance</SelectItem>
+                        <SelectItem value='STAGE'>Stage</SelectItem>
+                        <SelectItem value='ALTERNANCE'>Alternance</SelectItem>
                       </SelectContent>
                     </Select>
                   )}
                 />
               </FormControl>
               <FormMessage>
-                {methods.formState.errors.type && "Ce champ est obligatoire"}
+                {methods.formState.errors.type && 'Ce champ est obligatoire'}
               </FormMessage>
             </FormItem>
           )}
         />
         <FormField
           control={methods.control}
-          name="weekCount"
+          name='weekCount'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Nombre de semaines</FormLabel>
-                <FormControl>
-                <Input type="number" placeholder="Nombre de semaines" {...field} max={30} />
-                </FormControl>
+              <FormControl>
+                <Input
+                  type='number'
+                  placeholder='Nombre de semaines'
+                  {...field}
+                  max={30}
+                />
+              </FormControl>
               <FormMessage>
-                {methods.formState.errors.weekCount && "Ce champ est obligatoire"}
+                {methods.formState.errors.weekCount &&
+                  'Ce champ est obligatoire'}
               </FormMessage>
             </FormItem>
           )}
         />
         <FormField
           control={methods.control}
-          name="years"
+          name='years'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Année</FormLabel>
               <FormControl>
-                <Input type="number" placeholder="Année" {...field} />
+                <Input type='number' placeholder='Année' {...field} />
               </FormControl>
               <FormMessage>
-                {methods.formState.errors.years && "Ce champ est obligatoire"}
+                {methods.formState.errors.years && 'Ce champ est obligatoire'}
               </FormMessage>
             </FormItem>
           )}
         />
         <FormField
           control={methods.control}
-          name="comment"
+          name='comment'
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Commentaries</FormLabel>
+              <FormLabel>commentaires</FormLabel>
               <FormControl>
-                <Input placeholder="Commentaries" {...field} />
+                <Input placeholder='commentaires' {...field} />
               </FormControl>
             </FormItem>
           )}
         />
-        <div className="flex space-x-4">
+        <div className='flex space-x-4'>
           <button
-            className="px-4 py-2 my-4 bg-primary-blue text-white rounded hover:bg-primary-blue-hover transition-colors duration-300"
-            type="submit"
+            className='px-4 py-2 my-4 bg-primary-blue text-white rounded hover:bg-primary-blue-hover transition-colors duration-300'
+            type='submit'
           >
             Mettre à jour le stage
           </button>
           <button
-            className="px-4 py-2 my-4 bg-red-500 text-white rounded hover:bg-red-700 transition-colors duration-300"
-            type="button"
+            className='px-4 py-2 my-4 bg-red-500 text-white rounded hover:bg-red-700 transition-colors duration-300'
+            type='button'
             onClick={() => methods.reset()}
           >
             Réinitialiser
@@ -123,7 +149,7 @@ const InternshipPutForm: React.FC<InternshipPutFormProps> = ({ onSubmit, interns
         </div>
       </form>
     </FormProvider>
-  );
-};
+  )
+}
 
-export default InternshipPutForm;
+export default InternshipPutForm
